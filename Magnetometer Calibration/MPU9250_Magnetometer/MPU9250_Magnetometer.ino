448bf6895e65267ca7c5556be8e3dbd183bed940
Hongbo Zhu <49983377+DonovanZhu@users.noreply.github.com>
2020-08-10 12:03:22 -0400

    Create MPU9250_Magnetometer.ino

---
+++ Magnetometer Calibration/MPU9250_Magnetometer/MPU9250_Magnetometer.ino
@@ -0,0 +1,45 @@
+#include "MPU9250.h"
+MPU9250 IMU(Wire,0x68);
+
+float magnetom[3];
+
+void read_sensors() {
+  IMU.readSensor();
+  magnetom[0] = IMU.getMagX_uT();
+  magnetom[1] = IMU.getMagY_uT();
+  magnetom[2] = IMU.getMagZ_uT();
+}
+
+
+void sendToPC(float* data1, float* data2, float* data3)
+{
+  byte* byteData1 = (byte*)(data1);
+  byte* byteData2 = (byte*)(data2);
+  byte* byteData3 = (byte*)(data3);
+  byte buf[12] = {byteData1[0], byteData1[1], byteData1[2], byteData1[3],
+                 byteData2[0], byteData2[1], byteData2[2], byteData2[3],
+                 byteData3[0], byteData3[1], byteData3[2], byteData3[3]};
+  Serial.write(buf, 12);
+}
+
+void setup() {
+  Serial.begin(1000000);
+  while (!Serial) yield();
+
+  // start communication with IMU 
+  IMU.begin();
+  // setting the accelerometer full scale range to +/-8G 
+  IMU.setAccelRange(MPU9250::ACCEL_RANGE_4G);
+  // setting the gyroscope full scale range to +/-500 deg/s
+  IMU.setGyroRange(MPU9250::GYRO_RANGE_500DPS);
+  // setting DLPF bandwidth to 20 Hz
+  IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_41HZ);
+  // setting SRD to 19 for a 50 Hz update rate
+  IMU.setSrd(9);
+}
+
+void loop() {
+  read_sensors();
+  delay(10);
+  sendToPC(&magnetom[0], &magnetom[1], &magnetom[2]);
+}
