b3ebaa204210a5ccd5059939843a4f25fd4a389a
Hongbo Zhu <49983377+DonovanZhu@users.noreply.github.com>
2020-10-16 11:07:36 -0400

    Update Gyro_Acc_Calibration.py

--- Raspberry_pi/Calibration/Gyro_Acc_Calibration.py
+++ Raspberry_pi/Calibration/Gyro_Acc_Calibration.py
@@ -13,19 +13,13 @@ avg_acc  = np.array([0.0, 0.0, 0.0])
 
 loop_num = 1
 while True:
+    # read data and calculate the average
     acceleration = accel_gyro.acceleration
     gyro = accel_gyro.gyro
     avg_gyro += np.array(gyro)
     avg_acc  += np.array(acceleration)
-    #avg_gyro[0], avg_gyro[1], avg_gyro[2] += gyro[0], gyro[1], gyro[2]
-    #avg_acc[0], avg_acc[1], avg_acc[2] += acceleration[0], acceleration[1], acceleration[2]
+
     print(avg_acc / loop_num)
     print(avg_gyro / loop_num)
     print("")
     loop_num += 1
-
-    #time.sleep(0.5)
-
-# [ 0.06653735 -0.11510433 10.08015258]
-# [ 0.01383991  0.00135993 -0.00649945]
-
